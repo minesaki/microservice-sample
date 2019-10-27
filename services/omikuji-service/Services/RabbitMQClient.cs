934e7fa2433b9516d39c8adfd9dda2210eda6612
minesaki <45717310+minesaki@users.noreply.github.com>
2019-10-28 08:43:07 +0900

    RabbitMQへの接続情報を環境変数から取得

--- services/omikuji-service/Services/RabbitMQClient.cs
+++ services/omikuji-service/Services/RabbitMQClient.cs
@@ -9,12 +9,23 @@ public class RabbitMQClient
 {
     public void publish(string eventName)
     {
+        string rabbitMQHostName = Environment.GetEnvironmentVariable("MQ_HostName");
+        string rabbitMQUserName = Environment.GetEnvironmentVariable("MQ_UserName");
+        string rabbitMQPassword = Environment.GetEnvironmentVariable("MQ_Password");
+
+        if (string.IsNullOrWhiteSpace(rabbitMQHostName) ||
+            string.IsNullOrWhiteSpace(rabbitMQUserName) ||
+            string.IsNullOrWhiteSpace(rabbitMQPassword))
+        {
+            return;
+        }
+
         // いったんpublishの度に生成しているが、パフォーマンス的に良くないかも
         ConnectionFactory factory = new RabbitMQ.Client.ConnectionFactory
         {
-            HostName = "localhost",
-            UserName = "guest",
-            Password = "guest"
+            HostName = rabbitMQHostName,
+            UserName = rabbitMQUserName,
+            Password = rabbitMQPassword
         };
         using (IConnection con = factory.CreateConnection())
         using (IModel channel = con.CreateModel())
